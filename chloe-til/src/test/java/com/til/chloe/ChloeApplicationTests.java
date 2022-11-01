e6aee71473bad1225abd779248f445ae9cad993e
chloeakazhixian <70254576+chloeakazhixian@users.noreply.github.com>
2022-11-01 21:25:59 +0900

    init

---
+++ chloe-til/src/test/java/com/til/chloe/ChloeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.til.chloe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChloeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
